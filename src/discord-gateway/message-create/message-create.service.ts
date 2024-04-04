40a65597a4c94ef4f0ab245e0b00abe8b201207b
SlinkyPotato <patinobrian@gmail.com>
2024-04-04 10:28:22 -0400

    add custom message for ryan

--- src/discord-gateway/message-create/message-create.service.ts
+++ src/discord-gateway/message-create/message-create.service.ts
@@ -10,6 +10,11 @@ export class MessageCreateService {
   async onMessageCreate(message: Message): Promise<any> {
     try {
       if (message.author.bot) return;
+
+      if (message.author.id === '429026586570784770') { // ryan
+        return await message.react('we know how much you like toes ryan');
+      }
+
       if (message.content.match(/\s*toe/gi)
         ?? message.content.match(/\s*toes/gi)
         ?? message.content.match(/^\s*evertoe$/gi)) {
